9c49ba0d8ea0ddaf2342c068b29292bef4365b85
Ilya Kuydich <agree2012@yandex.ru>
2017-06-16 16:38:49 +0400

    Update Placeholders.java

--- src/main/java/com/tngtech/java/junit/dataprovider/Placeholders.java
+++ src/main/java/com/tngtech/java/junit/dataprovider/Placeholders.java
@@ -1,4 +1,4 @@
-package com.tngtech.java.junit.dataprovider;
+/*package com.tngtech.java.junit.dataprovider;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,22 +11,6 @@ import com.tngtech.java.junit.dataprovider.internal.placeholder.ParameterPlaceho
 import com.tngtech.java.junit.dataprovider.internal.placeholder.SimpleClassNamePlaceholder;
 import com.tngtech.java.junit.dataprovider.internal.placeholder.SimpleMethodNamePlaceholder;
 
-/**
- * Use this class to manipulate the generation/formatting of test method names.
- * <p>
- * E.g. one can add a new placeholder using a static initializer block in a base class of all tests:
- *
- * <pre>
- * <code>
- * public static class BaseTest {
- *     static {
- *         Placeholders.all().add(0, new MyFancyParameterPlaceholder());
- *     }
- *     // ...
- * }
- * </code>
- * </pre>
- */
 public class Placeholders {
 
     private static final List<BasePlaceholder> placeholders = new ArrayList<BasePlaceholder>();
@@ -34,27 +18,11 @@ public class Placeholders {
         reset();
     }
 
-    /**
-     * Retrieve all {@link BasePlaceholder} to handle {@link DataProvider#format()}. The returned {@link List} is the
-     * original list such that all manipulations will change the behavior how test method names are formatted.
-     * <p>
-     * <b>Note:</b>
-     * <ul>
-     * <li>The placeholder are process in order.</li>
-     * <li>The first matching placeholder wins, especially if an earlier processed placeholder is a substring of a later
-     * one (e.g. {@code %c} and {@code %cc})</li>
-     * </ul>
-     *
-     * @return all {@link BasePlaceholder}s to handle {@link DataProvider#format()} (not a copy!)
-     */
     public static List<BasePlaceholder> all() {
         return placeholders;
     }
 
-    /**
-     * Resets all changes to the list of all {@link BasePlaceholder} such that is contains the default placeholders
-     * again.
-     */
+   
     public static void reset() {
         placeholders.clear();
         placeholders.add(new CanonicalClassNamePlaceholder());
@@ -65,3 +33,4 @@ public class Placeholders {
         placeholders.add(new SimpleMethodNamePlaceholder());
     }
 }
+*/
