a8b93cdbc4c6adebf0fda5fa703f975b39b6e538
Ilya Kuydich <agree2012@yandex.ru>
2017-06-16 16:48:22 +0400

    Update Placeholders.java

--- src/main/java/com/tngtech/java/junit/dataprovider/Placeholders.java
+++ src/main/java/com/tngtech/java/junit/dataprovider/Placeholders.java
@@ -8,8 +8,8 @@ import com.tngtech.java.junit.dataprovider.internal.placeholder.CanonicalClassNa
 import com.tngtech.java.junit.dataprovider.internal.placeholder.CompleteMethodSignaturePlaceholder;
 import com.tngtech.java.junit.dataprovider.internal.placeholder.IndexPlaceholder;
 import com.tngtech.java.junit.dataprovider.internal.placeholder.ParameterPlaceholder;
-//import com.tngtech.java.junit.dataprovider.internal.placeholder.SimpleClassNamePlaceholder;
-//import com.tngtech.java.junit.dataprovider.internal.placeholder.SimpleMethodNamePlaceholder;
+import com.tngtech.java.junit.dataprovider.internal.placeholder.SimpleClassNamePlaceholder;
+import com.tngtech.java.junit.dataprovider.internal.placeholder.SimpleMethodNamePlaceholder;
 
 
 public class Placeholders {
@@ -31,7 +31,7 @@ public class Placeholders {
         placeholders.add(new CompleteMethodSignaturePlaceholder());
         placeholders.add(new IndexPlaceholder());
         placeholders.add(new ParameterPlaceholder());
-        //placeholders.add(new SimpleClassNamePlaceholder());
-        //placeholders.add(new SimpleMethodNamePlaceholder());
+        placeholders.add(new SimpleClassNamePlaceholder());
+        placeholders.add(new SimpleMethodNamePlaceholder());
     }
 }
