74c7a98eadb5bd435eeede0f21dfefa41c114af1
Ilya Kuydich <agree2012@yandex.ru>
2017-06-16 16:34:48 +0400

    Update CompleteMethodSignaturePlaceholder.java

--- src/main/java/com/tngtech/java/junit/dataprovider/internal/placeholder/CompleteMethodSignaturePlaceholder.java
+++ src/main/java/com/tngtech/java/junit/dataprovider/internal/placeholder/CompleteMethodSignaturePlaceholder.java
@@ -1,4 +1,4 @@
-package com.tngtech.java.junit.dataprovider.internal.placeholder;
+/*package com.tngtech.java.junit.dataprovider.internal.placeholder;
 
 public class CompleteMethodSignaturePlaceholder extends BasePlaceholder {
     public CompleteMethodSignaturePlaceholder() {
@@ -9,4 +9,4 @@ public class CompleteMethodSignaturePlaceholder extends BasePlaceholder {
     protected String getReplacementFor(String placeholder) {
         return method.toString();
     }
-}
+}*/
